f98b0c1fa6f95de4d9df7bca9fe792d708c774a0
Jean Chalard <jchalard@google.com>
2015-03-14 19:24:44 +0900

    Cut out a clean class for the actual work in the app
    Also add a visible log facility and a load button

--- Feed/src/main/java/com/j/jface/feed/JFaceDataFeed.java
+++ Feed/src/main/java/com/j/jface/feed/JFaceDataFeed.java
@@ -1,33 +1,12 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.j.jface.feed;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ComponentName;
-import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.wearable.companion.WatchFaceCompanion;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
@@ -39,93 +18,55 @@ import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
-/**
- * The phone-side config activity for {@code DigitalWatchFaceService}. Like the watch-side config
- * activity ({@code DigitalWatchFaceWearableConfigActivity}), allows for setting the background
- * color. Additionally, enables setting the color for hour, minute and second digits.
- */
-public class JFaceDataFeed extends Activity
- implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
- ResultCallback<DataApi.DataItemResult>
+public class JFaceDataFeed implements ResultCallback<DataApi.DataItemResult>
 {
-  private static final String TAG = "DigitalWatchFaceConfig";
-
-  // TODO: use the shared constants (needs covering all the samples with Gradle build model)
-  private static final String KEY_BACKGROUND_COLOR = "BACKGROUND_COLOR";
-  private static final String KEY_HOURS_COLOR = "HOURS_COLOR";
-  private static final String KEY_MINUTES_COLOR = "MINUTES_COLOR";
-  private static final String KEY_SECONDS_COLOR = "SECONDS_COLOR";
+  private static final String TAG = "Datafeed";
+  private static final String PEER_ID = "9705384f-ec27-4e68-b880-0dad6c2d9605";
   private static final String DATA_PATH = "/jwatch/Data";
 
-  private GoogleApiClient mGoogleApiClient;
-  private final String mPeerId = "9705384f-ec27-4e68-b880-0dad6c2d9605";
-  private EditText mDataEdit;
+  private final Activity mA;
+  private final EditText mDataEdit;
+  private final TextView mLog;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState)
-  {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_digital_watch_face_config);
-
-    mGoogleApiClient = new GoogleApiClient.Builder(this)
-     .addConnectionCallbacks(this)
-     .addOnConnectionFailedListener(this)
-     .addApi(Wearable.API)
-     .build();
-
-    ComponentName name = getIntent().getParcelableExtra(WatchFaceCompanion.EXTRA_WATCH_FACE_COMPONENT);
-    mDataEdit = (EditText) findViewById(R.id.dataItem);
+  public JFaceDataFeed(final Activity a) {
+    mA = a;
+    mA.setContentView(R.layout.activity_digital_watch_face_config);
+    mDataEdit = (EditText)mA.findViewById(R.id.dataItem);
+    mLog = (TextView)mA.findViewById(R.id.log);
   }
 
-  @Override
-  protected void onStart()
-  {
-    super.onStart();
-    mGoogleApiClient.connect();
+  public void Log(final String s) {
+    mLog.append(s);
+    mLog.append("\n");
   }
 
-  @Override
-  protected void onStop()
-  {
-    if (mGoogleApiClient != null && mGoogleApiClient.isConnected())
-    {
-      mGoogleApiClient.disconnect();
-    }
-    super.onStop();
+  public void onClickSet(final GoogleApiClient client, final View button) {
+    setValue(client, mDataEdit.getText().toString());
   }
 
-  @Override // GoogleApiClient.ConnectionCallbacks
-  public void onConnected(Bundle connectionHint)
-  {
-    Log.d(TAG, "onConnected: " + connectionHint);
-
-    if (mPeerId != null)
-    {
-      Uri.Builder builder = new Uri.Builder();
-      Uri uri = builder.scheme("wear").path(DATA_PATH).authority(mPeerId).build();
-      Wearable.DataApi.getDataItem(mGoogleApiClient, uri).setResultCallback(this);
-    }
-    else
-    {
-      displayNoConnectedDeviceDialog();
-    }
-  }
-
-  public void onClickSet(final View v) {
-    Log.e("CLICK", "set : " + mDataEdit.getText().toString());
-    setValue(mDataEdit.getText().toString());
-  }
-
-  private void setValue(final String value) {
+  private void setValue(final GoogleApiClient client, final String value) {
     PutDataMapRequest putDataMapReq = PutDataMapRequest.create(DATA_PATH);
     putDataMapReq.getDataMap().putString("foobar", value);
     PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
-    PendingResult<DataApi.DataItemResult> pendingResult =
-     Wearable.DataApi.putDataItem(mGoogleApiClient, putDataReq);
+    PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi.putDataItem(client, putDataReq);
   }
 
-  @Override // ResultCallback<DataApi.DataItemResult>
-  public void onResult(DataApi.DataItemResult dataItemResult)
+  public void load() {
+    Log("clicked");
+  }
+
+
+  // GoogleApiClient.ConnectionCallbacks
+  public void onConnected(final GoogleApiClient client, final Bundle connectionHint)
+  {
+    Uri.Builder builder = new Uri.Builder();
+    Uri uri = builder.scheme("wear").path(DATA_PATH).authority(PEER_ID).build();
+    Wearable.DataApi.getDataItem(client, uri).setResultCallback(this);
+  }
+
+  // ResultCallback
+  @Override
+  public void onResult(final DataApi.DataItemResult dataItemResult)
   {
     if (dataItemResult.getStatus().isSuccess() && dataItemResult.getDataItem() != null)
     {
@@ -135,49 +76,12 @@ public class JFaceDataFeed extends Activity
     }
   }
 
-  @Override // GoogleApiClient.ConnectionCallbacks
-  public void onConnectionSuspended(int cause)
-  {
-    if (Log.isLoggable(TAG, Log.DEBUG))
-    {
-      Log.d(TAG, "onConnectionSuspended: " + cause);
-    }
-  }
-
-  @Override // GoogleApiClient.OnConnectionFailedListener
-  public void onConnectionFailed(ConnectionResult result)
-  {
-    if (Log.isLoggable(TAG, Log.DEBUG))
-    {
-      Log.d(TAG, "onConnectionFailed: " + result);
-    }
-  }
-
-  private void displayNoConnectedDeviceDialog()
-  {
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    String messageText = getResources().getString(R.string.title_no_device_connected);
-    String okText = getResources().getString(R.string.ok_no_device_connected);
-    builder.setMessage(messageText)
-     .setCancelable(false)
-     .setPositiveButton(okText, new DialogInterface.OnClickListener()
-     {
-       public void onClick(DialogInterface dialog, int id)
-       {
-       }
-     });
-    AlertDialog alert = builder.create();
-    alert.show();
-  }
-
-  private void sendConfigUpdateMessage(String configKey, String msg)
-  {
-    if (mPeerId != null)
-    {
-      DataMap config = new DataMap();
-      config.putString(configKey, msg);
-      byte[] rawData = config.toByteArray();
-      Wearable.MessageApi.sendMessage(mGoogleApiClient, mPeerId, DATA_PATH, rawData);
-    }
-  }
+  // Save for the future, in case we need it
+//  private void sendConfigUpdateMessage(final String configKey, final String msg)
+//  {
+//    final DataMap config = new DataMap();
+//    config.putString(configKey, msg);
+//    final byte[] rawData = config.toByteArray();
+//    Wearable.MessageApi.sendMessage(mClient, PEER_ID, DATA_PATH, rawData);
+//  }
 }
