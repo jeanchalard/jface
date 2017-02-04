bf6e66468b50eede398248fcb6c4d43639ab1e72
J <jean.chalard@gmail.com>
2017-02-04 03:56:09 -0800

    Convert the list display to a Recycler view.

--- Feed/src/main/java/com/j/jface/org/JOrg.java
+++ Feed/src/main/java/com/j/jface/org/JOrg.java
@@ -1,15 +1,12 @@
 package com.j.jface.org;
 
-import android.animation.LayoutTransition;
-import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import com.j.jface.R;
@@ -20,7 +17,6 @@ import com.j.jface.org.todo.TodoSource;
 import com.j.jface.org.todo.TodoUtil;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Main activity class for JOrg.
@@ -32,9 +28,7 @@ public class JOrg extends WrappedActivity
   @NonNull private final TodoSource mTodoSource;
   @NonNull private final SoundSource mSoundSource;
   @NonNull private final EditTextSoundRouter mSoundRouter;
-  // This is final but nowhere near unmodifiable ; in fact it's modified all over
-  // the place whenever the UI changes anything.
-  @NonNull private final ArrayList<Todo> mTodoList;
+  @NonNull private final TodoAdapter mAdapter;
 
   public JOrg(@NonNull Args args)
   {
@@ -42,23 +36,21 @@ public class JOrg extends WrappedActivity
     mA.setContentView(R.layout.org_top);
     mClient = new Client(mA);
     mTodoSource = new TodoSource(mA);
-    mSoundSource = new SoundSource(mA, (ViewGroup) mA.findViewById(R.id.sound_source));
+    mSoundSource = new SoundSource(mA, (ViewGroup)mA.findViewById(R.id.sound_source));
     mSoundRouter = new EditTextSoundRouter(mSoundSource);
+    ArrayList<Todo> todoList = mTodoSource.fetchTodoList();
+    mAdapter = new TodoAdapter(mA, mSoundRouter, todoList);
 
-    final LinearLayout top = (LinearLayout)mA.findViewById(R.id.todoList);
-    top.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
-    top.getLayoutTransition().setDuration(LAYOUT_ANIMATION_DURATION);
-
-    mTodoList = mTodoSource.fetchTodoList();
-    addTodoViews(mTodoList, top, 0);
+    final RecyclerView rv = (RecyclerView) mA.findViewById(R.id.todoList);
+    rv.setAdapter(mAdapter);
 
     final FloatingActionButton fab = (FloatingActionButton)mA.findViewById(R.id.addTodo);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view)
       {
-        final Todo newTodo = new Todo("");
-        mTodoList.add(newTodo);
-        addTodoView(newTodo, top, 0);
+//        final Todo newTodo = new Todo("");
+//        mTodoList.add(newTodo);
+//        addTodoView(newTodo, top, 0);
       }
     });
   }
@@ -71,58 +63,6 @@ public class JOrg extends WrappedActivity
   public void onResume()
   {
     mSoundSource.onResume();
-  }
-
-  private void addTodoViews(final List<Todo> l, final LinearLayout topView, final int shift)
-  {
-    for (final Todo todo : l)
-    {
-      addTodoView(todo, topView, shift);
-      addTodoViews(todo.mChildren, topView, shift + 25);
-    }
-  }
-
-  private void addTodoView(final Todo todo, final LinearLayout topView, final int shift)
-  {
-    topView.addView(inflateTodo(todo, shift));
-    final View separator = new View(mA);
-    separator.setBackgroundColor(0xFFA0A0A0);
-    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 1);
-    lp.setMargins(20 + shift, 0, 20, 0);
-    separator.setLayoutParams(lp);
-    topView.addView(separator);
-  }
-
-  private View inflateTodo(final Todo t, final int shift)
-  {
-    @SuppressLint("InflateParams") final LinearLayout v = (LinearLayout)mA.getLayoutInflater().inflate(R.layout.todo, null);
-    final SelReportEditText et = ((SelReportEditText)v.findViewById(R.id.todoText));
-    et.setOnFocusChangeListener(mSoundRouter);
-    et.mListener = mSoundRouter;
-    final LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-    lp.setMargins(shift, 0, 0, 0);
-    v.setLayoutParams(lp);
-    final LayoutTransition lt = v.getLayoutTransition();
-    lt.setDuration(LayoutTransition.CHANGE_APPEARING, LAYOUT_ANIMATION_DURATION);
-    lt.setDuration(LayoutTransition.CHANGE_DISAPPEARING, LAYOUT_ANIMATION_DURATION);
-    lt.setStartDelay(LayoutTransition.APPEARING, 0);
-    et.setText(t.mText);
-    final View expansion = v.findViewById(R.id.todoExpanded);
-    final ImageButton b = (ImageButton)v.findViewById(R.id.todoExpandButton);
-    b.setOnClickListener(new View.OnClickListener()
-    {
-      @Override
-      public void onClick(final View clicked)
-      {
-        if (expansion.getVisibility() == View.VISIBLE)
-          expansion.setVisibility(View.GONE);
-        else
-          expansion.setVisibility(View.VISIBLE);
-      }
-    });
-    expansion.setVisibility(View.GONE);
-    populateExpandedTodo(v, t);
-    return v;
   }
 
   private void populateExpandedTodo(final View v, final Todo t)
