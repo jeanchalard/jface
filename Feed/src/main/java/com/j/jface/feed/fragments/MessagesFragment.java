af5543e6647b88332a04e3c26e7b7252fe36a3ca
J <jean.chalard@gmail.com>
2017-04-15 14:43:04 +0900

    Add a color picker to the message setter, and remove the button.
    The button is of no use, we should just send the text whenever it
    changes. The color picker will be useful to combat message blindness.

--- Feed/src/main/java/com/j/jface/feed/fragments/MessagesFragment.java
+++ Feed/src/main/java/com/j/jface/feed/fragments/MessagesFragment.java
@@ -2,52 +2,64 @@ package com.j.jface.feed.fragments;
 
 import android.app.Fragment;
 import android.support.annotation.NonNull;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.android.gms.wearable.DataMap;
 import com.j.jface.Const;
 import com.j.jface.R;
 import com.j.jface.client.Client;
+import com.j.jface.feed.views.PaletteView;
 import com.j.jface.lifecycle.WrappedFragment;
 
 /**
  * A fragment for showing and managing arbitrary messages.
  */
-public class MessagesFragment extends WrappedFragment implements View.OnClickListener
+public class MessagesFragment extends WrappedFragment implements TextWatcher, PaletteView.OnColorSetListener
 {
   @NonNull private final Fragment mF;
   @NonNull private final Client mClient;
   @NonNull private final EditText mTopicDataEdit;
+  @NonNull private final PaletteView mPalette;
+  private boolean mDataFetched; // Replace this if this class or the use of this member ever become any more than elementary
 
   public MessagesFragment(@NonNull final WrappedFragment.Args a, @NonNull final Client b)
   {
     super(a.inflater.inflate(R.layout.fragment_messages, a.container, false));
     mF = a.fragment;
     mClient = b;
-    mTopicDataEdit = (EditText)mView.findViewById(R.id.topicDataEdit);
-    mView.findViewById(R.id.button_set).setOnClickListener(this);
+    mTopicDataEdit = (EditText)mView.findViewById(R.id.messagesFragment_topicDataEdit);
+    mTopicDataEdit.addTextChangedListener(this);
+    mPalette = (PaletteView)mView.findViewById(R.id.messagesFragment_palette);
+    mPalette.addOnColorSetListener(this);
+    mDataFetched = false;
     b.getData(Const.DATA_PATH + "/" + Const.DATA_KEY_TOPIC, new Client.GetDataCallback() {
       public void run(@NonNull final String path, @NonNull final DataMap dataMap) {
         a.fragment.getActivity().runOnUiThread(new Runnable() {
           @Override
           public void run() {
             mTopicDataEdit.setText(dataMap.getString(Const.DATA_KEY_TOPIC));
+            mDataFetched = true;
           }
         });
       }
     });
   }
 
-  @Override public void onClick(final View v)
+  @Override public void afterTextChanged(@NonNull final Editable s)
   {
-    switch (v.getId())
-    {
-      case R.id.button_set:
-        mClient.putData(Const.DATA_PATH + "/" + Const.DATA_KEY_TOPIC, Const.DATA_KEY_TOPIC, mTopicDataEdit.getText().toString());
-        break;
-      default:
-        // Nothing, how did you come here ?
-    }
+    if (!mDataFetched) return;
+    mClient.putData(Const.DATA_PATH + "/" + Const.DATA_KEY_TOPIC, Const.DATA_KEY_TOPIC, mTopicDataEdit.getText().toString());
+  }
+
+  @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+  @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+  @Override public void onColorSet(final int color)
+  {
+    mTopicDataEdit.setTextColor(color);
   }
 }
---
+++ Feed/src/main/java/com/j/jface/feed/views/PaletteView.java
@@ -0,0 +1,45 @@
+package com.j.jface.feed.views;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class PaletteView extends ImageView
+{
+  @NonNull final private Bitmap mPaletteBitmap;
+  @NonNull final private Matrix mImageMatrix;
+  public interface OnColorSetListener { public void onColorSet(final int color); }
+  public PaletteView(@NonNull final Context context) { this(context, null, 0); }
+  public PaletteView(@NonNull final Context context, @Nullable final AttributeSet attrs) { this(context, attrs, 0); }
+  public PaletteView(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr)
+  {
+    super(context, attrs, defStyleAttr);
+    mPaletteBitmap = ((BitmapDrawable)getDrawable()).getBitmap();
+    mImageMatrix = new Matrix();
+    getImageMatrix().invert(mImageMatrix);
+  }
+
+  @NonNull final ArrayList<OnColorSetListener> mListeners = new ArrayList<>();
+  public void addOnColorSetListener(@NonNull final OnColorSetListener listener) { mListeners.add(listener); }
+  public void removeOnSetColorSetListener(@NonNull final OnColorSetListener listener) { mListeners.remove(listener); }
+
+  @Override public boolean onTouchEvent(@NonNull final MotionEvent event)
+  {
+    Log.e("Touch", "" + event.getX() + " x " + event.getY());
+    final float[] pts = new float[] { event.getX(), event.getY() };
+    mImageMatrix.mapPoints(pts);
+    if (pts[0] < 0 || pts[1] < 0 || pts[0] >= mPaletteBitmap.getWidth() || pts[1] >= mPaletteBitmap.getHeight()) return true;
+    final int color = mPaletteBitmap.getPixel((int)pts[0], (int)pts[1]);
+    for (final OnColorSetListener listener : mListeners) listener.onColorSet(color);
+    return true;
+  }
+}
