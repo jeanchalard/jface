f67f77fc53bed3d4c79b868e4d33f502e4d8c454
J <jean.chalard@gmail.com>
2018-08-06 09:44:06 +0800

    Change the Hibiya parser for the new site format
    Tōkyō Metro changed the format of their HTML pages.
    Adjust the parser.

--- Feed/src/main/java/com/j/jface/feed/HibiyaParser.java
+++ Feed/src/main/java/com/j/jface/feed/HibiyaParser.java
@@ -9,33 +9,37 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HibiyaParser extends FeedParser
 {
   @Override @NonNull
-  public DataMap parseStream(@NonNull final String dataName, @NonNull final BufferedInputStream srcStream) throws IOException
+  public DataMap parseStream(@NonNull final String dataName, @NonNull final BufferedInputStream srcStream) throws IOException, ParseException
   {
     final DataMap result = new DataMap();
     final BufferedReader src = new BufferedReader(new InputStreamReader(srcStream));
-    if (null == find(src, "table summary=\"平日の時刻表\"") || null == find(src, "</tr>"))
-      return result;
-    final Scanner srcTable = new Scanner(find(src, "</table>"));
+    if (null == find(src, "table class=\"v2_table "))
+      throw new ParseException("HibiyaParser : can't parse page " + dataName, 0);
+    final Scanner srcTable = new Scanner(src);
     final ArrayList<DataMap> buildData = new ArrayList<>();
-    srcTable.useDelimiter("(\\s|<|>)+");
+    srcTable.useDelimiter("(<|>)+");
     int hour = -1;
     int minute = -1;
     boolean 始発 = false;
     while (srcTable.hasNext()) {
       final String p = srcTable.next();
-      if (p.startsWith("class=\"hour\""))
+      if ("th".equals(p))
         hour = srcTable.nextInt();
-      else if ("class=\"item02\"".equals(p) && "●".equals(srcTable.next()))
+      else if ("span class=\"v2_tableTimeTxt\"".equals(p) && srcTable.next().contains("●"))
         始発 = true;
-      else if ("class=\"info02\"".equals(p))
+      else if (p.startsWith("a href=\"/station/timetable.html"))
         minute = srcTable.nextInt();
-      else if (p.startsWith("/p")) {
+      else if ("/table".equals(p))
+        break;
+      else if (p.startsWith("/li"))
+      {
         final DataMap departure = new DataMap();
         departure.putInt(Const.DATA_KEY_DEPTIME, hour * 3600 + minute * 60);
         departure.putString(Const.DATA_KEY_EXTRA, 始発 ? "始" : "");
